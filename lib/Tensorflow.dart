dd383c4b7b4d42f6f065aa20c988204819440344
Deshal-001 <kavindudeshal2@gmail.com>
2022-03-22 14:33:22 +0530

    Updated

---
+++ lib/Tensorflow.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Tensorflow extends StatefulWidget {
+  const Tensorflow({Key? key}) : super(key: key);
+
+
+  @override
+  _TensorflowState createState() => _TensorflowState();
+}
+
+class _TensorflowState extends State<Tensorflow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
